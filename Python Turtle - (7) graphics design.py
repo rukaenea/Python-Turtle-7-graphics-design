b9ac7f236e1ecd5a801d038344c7a15171985c58
Enea Ruka <140208753+rukaenea@users.noreply.github.com>
2023-08-03 09:46:49 +0200

    Add files via upload

---
+++ Python Turtle - (7) graphics design.py
@@ -0,0 +1,11 @@
+import turtle as t
+t.pencolor("cyan")
+t.bgcolor("black")
+t.goto(-200,-100)
+t.speed(999999)
+for i in range(99):
+	t.goto(-50,-100)
+	t.circle(-100,-170)
+	t.circle(-80,-900)
+	t.circle(-150,4500)
+	t.circle(-290,1500)
